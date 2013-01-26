87cb7de9c9f4309507bd6a205d96d7144949b0ae
Edward Wallitt <edwallitt@me.com>
2013-01-26 12:01:17 +0000

    initial commit

---
+++ oportfolio_iphone/OPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OPAppDelegate.h
+//  oportfolio_iphone
+//
+//  Created by Edward Wallitt on 26/01/2013.
+//  Copyright (c) 2013 Edward Wallitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ oportfolio_iphone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  oportfolio_iphone
+//
+//  Created by Edward Wallitt on 26/01/2013.
+//  Copyright (c) 2013 Edward Wallitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OPAppDelegate class]));
+    }
+}
